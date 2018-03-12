64bc84406d5b3c3c590262a10b2e102ef46cfa99
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-03-12 11:11:09 -0700

    More minor pylint updates

--- dist.py
+++ dist.py
@@ -20,12 +20,12 @@ from shutil import move
 
 def replace(file_path, pattern, subst):
     # Create temp file
-    handle, abs_path = mkstemp()
+    fh, abs_path = mkstemp()
     with open(abs_path, 'w') as new_file:
         with open(file_path) as old_file:
             for line in old_file:
                 new_file.write(line.replace(pattern, subst))
-    os.close(handle)
+    os.close(fh)
     # Remove original file
     os.remove(file_path)
     # Move new file
